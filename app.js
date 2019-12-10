7b75dbbd1e6d04afcd91b6ec537d8453dbd694b1
coder3-seekforwings <newmanwirl@126.com>
2019-12-10 17:20:29 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,2 @@
-// test_edit_by_newmandirl_6
-// test_edit_by_newmandirl_5
-// test_edit_by_newmandirl_4
-// test_edit_by_newmandirl_3
-// test_edit_by_newmandirl_2
-//test_edit_by_newmandirl
-//test_edit_by_newmandirl_0
-//test_edit_by_newmandirl_1
-//test_edit_by_newmandirl_2
-//test_edit_by_newmandirl_3
 //app.js
 App({})
